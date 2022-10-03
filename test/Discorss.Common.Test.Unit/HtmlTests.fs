75cef03e805410203681f44ea7d0423f1366438b
tonycknight <tonycknight@users.noreply.github.com>
2022-10-03 16:01:36 +0100

    strip html

---
+++ test/Discorss.Common.Test.Unit/HtmlTests.fs
@@ -0,0 +1,19 @@
+﻿namespace Discorss.Feeds.Test.Unit
+
+open System
+open Discorss
+open FsCheck
+open FsCheck.Xunit
+open FsUnit
+
+module HtmlTests=
+
+    [<Xunit.Theory>]
+    [<Xunit.InlineData("<body><table><th>test</th></table>yadda</body>", "yadda")>]
+    [<Xunit.InlineData("<body><script>do nothing</script>yadda</body>", "yadda")>]
+    let ``stripHtml returns inner text`` html expected =
+        let txt = Discorss.Html.stripHtml html
+        
+        txt |> Option.map id
+            |> Option.defaultValue ""
+            |> should equal expected
