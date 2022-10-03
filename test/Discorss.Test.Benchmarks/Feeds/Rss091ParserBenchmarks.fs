df8cd8a997293567ddf6bc5a74cc36ae6ff05fd0
tonycknight <tonycknight@users.noreply.github.com>
2022-10-03 15:06:28 +0100

    Rss 091 & 092 benchmarks

---
+++ test/Discorss.Test.Benchmarks/Feeds/Rss091ParserBenchmarks.fs
@@ -0,0 +1,42 @@
+﻿namespace Discorss.Test.Benchmarks.Feeds
+
+open System.Xml.Linq
+open BenchmarkDotNet.Attributes
+open Discorss.Feeds
+
+[<MemoryDiagnoserAttribute>]
+[<AllStatisticsColumnAttribute>]
+[<RankColumn>]
+[<JsonExporterAttribute.Full>]
+[<GcServerAttribute(true)>]
+type Rss091ParserBenchmarks()=
+    
+
+    [<IterationSetup>]
+    member this.IterationSetup()=
+        this.Text <- Discorss.Feeds.Test.Unit.TestHelpers.sampleFeedAsString this.SampleFileName
+        this.Doc <- this.Text |> XDocument.Parse
+        
+    [<Params("Rss091Feed.xml")>]
+    member val SampleFileName = "" with get, set
+
+    member val Text = "" with get, set
+    member val Doc = new XDocument() with get, set
+
+    [<Benchmark>]
+    member this.Parse()=
+        this.Doc
+            |> Rss20Parser.parse "http://url" 
+            |> ignore
+
+    [<Benchmark>]
+    member this.ParseWithLoad()=
+        this.Text 
+            |> XDocument.Parse
+            |> Rss20Parser.parse "http://url" 
+            |> ignore
+
+
+
+
+
