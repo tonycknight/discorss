c8155d3b18dfde713e412ee40db8173f40b6669f
tonycknight <tonycknight@users.noreply.github.com>
2022-10-03 15:47:06 +0100

    common tests shell project

---
+++ test/Discorss.Common.Test.Unit/Tests.fs
@@ -0,0 +1,12 @@
+﻿namespace Discorss.Feeds.Test.Unit
+
+open System
+open System.Xml.Linq
+open FsCheck
+open FsCheck.Xunit
+open FsUnit
+
+module Tests=
+    [<Xunit.Fact>]
+    let ``test``() =
+        ignore 0
