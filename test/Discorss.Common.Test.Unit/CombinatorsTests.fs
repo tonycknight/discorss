e8027b245293091a0606395edd768f7f3799965c
tonycknight <tonycknight@users.noreply.github.com>
2022-10-04 23:38:26 +0100

    more tests

--- test/Discorss.Common.Test.Unit/CombinatorsTests.fs
+++ test/Discorss.Common.Test.Unit/CombinatorsTests.fs
@@ -12,11 +12,27 @@ module CombinatorsTests=
     [<Xunit.InlineData(true, true, true)>]
     [<Xunit.InlineData(false, true, false)>]
     [<Xunit.InlineData(true, false, false)>]
+    [<Xunit.InlineData(false, false, false)>]
     let ``>&&> ``(left, right, expected)=
         let a = fun (x) -> left
         let b = fun (x) -> right
 
         let f = a >&&> b
+
+        let result = f 1
+
+        result |> should equal expected
+
+    [<Xunit.Theory>]
+    [<Xunit.InlineData(true, true, true)>]
+    [<Xunit.InlineData(false, true, true)>]
+    [<Xunit.InlineData(true, false, true)>]
+    [<Xunit.InlineData(false, false, false)>]
+    let ``>||> ``(left, right, expected)=
+        let a = fun (x) -> left
+        let b = fun (x) -> right
+
+        let f = a >||> b
 
         let result = f 1
 
---
+++ test/Discorss.Indexing.Test.Unit/LexiconsTests.fs
@@ -0,0 +1,28 @@
+﻿namespace Discorss.Indexing.Tests.Unit
+
+open System
+open Discorss
+open Discorss.Indexing
+open FsUnit
+open Xunit
+
+module LexiconsTests=
+
+    [<Fact>]
+    let ``isStopWord mapped to upper are all true on known members``()=
+        
+        let hits = Lexicons.stopWords
+                    |> Seq.map Strings.upper
+                    |> Seq.map Lexicons.isStopWord
+
+        hits |> Seq.exists (fun x -> not x ) |> should equal false
+
+    [<Fact>]
+    let ``isStopWord mapped to random are all false``()=
+        
+        let hits = Lexicons.stopWords
+                    |> Seq.map (fun _ -> System.Guid.NewGuid().ToString())
+                    |> Seq.map Lexicons.isStopWord
+
+        hits |> Seq.exists (fun x -> x ) |> should equal false
+
