48939d045985eda2472968ed04faa4d68560ef1b
tonycknight <tonycknight@users.noreply.github.com>
2022-10-04 20:40:42 +0100

    tests

---
+++ test/Discorss.Common.Test.Unit/CombinatorsTests.fs
@@ -0,0 +1,23 @@
+﻿namespace Discorss.Feeds.Test.Unit
+
+open System
+open Discorss
+open FsCheck
+open FsCheck.Xunit
+open FsUnit
+
+module CombinatorsTests=
+
+    [<Xunit.Theory>]
+    [<Xunit.InlineData(true, true, true)>]
+    [<Xunit.InlineData(false, true, false)>]
+    [<Xunit.InlineData(true, false, false)>]
+    let ``>&&> ``(left, right, expected)=
+        let a = fun (x) -> left
+        let b = fun (x) -> right
+
+        let f = a >&&> b
+
+        let result = f 1
+
+        result |> should equal expected
---
+++ test/Discorss.Common.Test.Unit/SeqTests.fs
@@ -0,0 +1,50 @@
+﻿namespace Discorss.Feeds.Test.Unit
+
+open System
+open Discorss
+open FsCheck
+open FsCheck.Xunit
+open FsUnit
+
+module SeqTests=
+    
+    [<Xunit.Fact>]
+    let ``noneToEmpty yields empty``()=        
+        let r = None |> Seq.noneToEmpty
+
+        r |> should equal Seq.empty
+
+    [<Xunit.Fact>]
+    let ``noneToEmpty yields sequence``()=
+        let xs = seq { 1 } 
+
+        let r = xs |> Some |> Seq.noneToEmpty
+        
+        r |> should equal xs
+
+
+    [<Property(Verbose = true)>]
+    let ``counts same value``(PositiveInt count)=
+        
+        let xs = [ 1 .. count ] |> Seq.map (fun _ -> 0) 
+
+        let result = xs |> Seq.counts |> Array.ofSeq
+
+        result |> should haveLength 1
+        result |> Seq.head |> snd |> should equal count
+
+        true
+
+    [<Property(Verbose = true)>]
+    let ``counts different values``(PositiveInt count)=
+        
+        let xs = [ 1 .. count ] |> Seq.collect (fun x -> [ x; x ]) 
+
+        let result = xs |> Seq.counts |> Array.ofSeq
+
+        result |> should haveLength count
+        result |> Seq.map snd |> Seq.min |> should equal 2
+        result |> Seq.map snd |> Seq.max |> should equal 2
+
+        true
+
