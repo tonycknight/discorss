4615130348ea05eee5088894fc92b7d43b54d2bf
tonycknight <tonycknight@users.noreply.github.com>
2022-10-09 10:35:50 +0100

    more mongo

---
+++ src/Discorss.Common/Mongo.fs
@@ -0,0 +1,20 @@
+﻿namespace Discorss
+
+open System
+open MongoDB.Driver
+
+module Mongo=
+
+    let dbClient(connection: string)=
+        let settings = connection |> MongoClientSettings.FromConnectionString
+
+        settings.AllowInsecureTls <- false
+        settings.UseTls <- true
+        settings.ConnectTimeout <- TimeSpan.FromSeconds(15)
+        settings.ServerSelectionTimeout <- settings.ConnectTimeout
+
+        new MongoClient(settings) :> IMongoClient
+        
+    let db (client: IMongoClient) name =
+        client.GetDatabase name
+
