5250212a53e2757ebad5b40aea51f4a9f334be65
tonycknight <tonycknight@users.noreply.github.com>
2022-10-10 23:58:30 +0100

    task map

--- src/Discorss.Common/Combinators.fs
+++ src/Discorss.Common/Combinators.fs
@@ -8,3 +8,10 @@ module Combinators=
 
     let (>||>) x y = 
         (fun (v: 'a) -> x(v) || y(v))
+
+module Task=
+    open System.Threading.Tasks
+
+    let map(f: 'a -> 'b) (x: Task<'a>)=
+        x.GetAwaiter().GetResult() |> f // TODO: ewww        
+
