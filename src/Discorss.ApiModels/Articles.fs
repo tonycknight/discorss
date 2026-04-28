196823474d98ed544034c1a19364a7af47a17aab
Tony Knight <tonycknight@users.noreply.github.com>
2026-04-28 22:56:04 +0100

    add APIModels project

---
+++ src/Discorss.ApiModels/Articles.fs
@@ -0,0 +1,11 @@
+﻿namespace Discorss.ApiModels
+
+[<CLIMutable>]
+type ArticleRequest =
+    { uri: string
+      title: string
+      description: string
+      author: string
+      content: string
+      categories: string[] }
+
