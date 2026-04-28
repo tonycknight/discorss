4733d0fdc12eff586d2d41d10b8e2a584821e6f0
Tony Knight <tonycknight@users.noreply.github.com>
2026-04-28 23:20:18 +0100

    more api models

---
+++ src/Discorss.ApiModels/Feeds.fs
@@ -0,0 +1,30 @@
+namespace Discorss.ApiModels
+
+open System
+
+type FeedType =
+    | Unknown = 0
+    | Rss20 = 1
+    | Rss091 = 2
+    | Rss092 = 3
+    | Atom  = 4
+
+[<CLIMutable>]
+type FeedInfo =
+    { uri: string
+      feedType: FeedType
+      title: string
+      description: string
+      updated: DateTimeOffset
+      lastFetched: DateTimeOffset }
+
+[<CLIMutable>]
+type FeedEntry =
+    { id: string
+      publication: DateTimeOffset
+      uri: string
+      title: string
+      description: string
+      author: string
+      content: string
+      categories: string list }
