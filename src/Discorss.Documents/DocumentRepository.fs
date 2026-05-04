f64ae1dadf95bcf3bea5a8ce8165fa5f50083f86
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-04 11:35:57 +0100

    tweawk doc repo signature

--- src/Discorss.Documents/DocumentRepository.fs
+++ src/Discorss.Documents/DocumentRepository.fs
@@ -1,14 +1,24 @@
 namespace Discorss.Documents
 
 open System.Threading.Tasks
+open Discorss
+open Microsoft.Extensions.Options
 
 type IDocumentRepository =
-    abstract member SetDocumentAsync: Document -> Task<unit>
+    abstract member SetDocumentAsync: Document -> Task<Document>
     abstract member GetDocumentAsync: string -> Task<Document option>
 
 type StubDocumentRepository() =
 
     interface IDocumentRepository with
-        member this.SetDocumentAsync(value: Document) = task { ignore 0 }
+        member this.SetDocumentAsync(value: Document) = task { return value }
+
+        member this.GetDocumentAsync(value: string) = task { return None }
+
+type MongoDocumentRepository(config: IOptions<AppConfiguration>) =
+    // TODO: 
+        
+    interface IDocumentRepository with
+        member this.SetDocumentAsync(value: Document) = task { return value }
 
         member this.GetDocumentAsync(value: string) = task { return None }
