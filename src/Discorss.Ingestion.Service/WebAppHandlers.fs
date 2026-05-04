a48ce07678f25c3c24d5486ca413d725fcb538b5
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-04 22:04:41 +0100

    refactoring

--- src/Discorss.Ingestion.Service/WebAppHandlers.fs
+++ src/Discorss.Ingestion.Service/WebAppHandlers.fs
@@ -10,27 +10,17 @@ open Giraffe
 
 module WebAppHandlers =
 
-    let ingestionActor (sp: IServiceProvider) =
-        sp.GetRequiredService<Ingestion.IngestionActor>() :> IActor
-
-    let feedActor (sp: IServiceProvider) =
-        sp.GetRequiredService<Ingestion.FeedIngestionActor>() :> IActor
-
-    let docActor (sp: IServiceProvider) =
-        sp.GetRequiredService<Ingestion.DocumentIngestionActor>() :> IActor
-
-    let queueActor (sp: IServiceProvider) =
-        sp.GetRequiredService<Ingestion.QueueMonitorActor>() :> IActor
-
     let getActorStats (sp: IServiceProvider) =
 
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let results =
-                    [| (ingestionActor sp).GetStats()
-                       (docActor sp).GetStats()
-                       (queueActor sp).GetStats()
-                       (feedActor sp).GetStats() |]
+                let actors =
+                    [| sp.GetRequiredService<Ingestion.QueueMonitorActor>() :> IActor
+                       sp.GetRequiredService<Ingestion.IngestionActor>() :> IActor
+                       sp.GetRequiredService<Ingestion.FeedIngestionActor>() :> IActor
+                       sp.GetRequiredService<Ingestion.DocumentIngestionActor>() :> IActor |]
+
+                let results = actors |> Array.map _.GetStats() |> Array.sortBy (fun x -> x.name)
 
                 return! Successful.OK results next ctx
             }
