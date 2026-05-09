b35f36e8f43da228f404ba7c57b5d4c2f819f625
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-09 22:35:18 +0100

    more tests

--- test/Discorss.Ingestion.Test.Unit/ModelsTests.fs
+++ test/Discorss.Ingestion.Test.Unit/ModelsTests.fs
@@ -8,16 +8,20 @@ open FsCheck.Xunit
 open FsUnit.Xunit
 
 module ModelsTests =
-    
+
     [<Property>]
-    let ``sha512 returns non-empty string`` (value: FeedEntry)=
+    let ``sha512 returns non-empty string`` (value: FeedEntry) =
         let result = Models.sha512 value
-        
+
         result |> Strings.isEmptyWhitespace |> not
 
     [<Property>]
+    let ``sha512 returns stable string`` (value: FeedEntry) =
+        Models.sha512 value = Models.sha512 value
+
+    [<Property>]
     let ``toDocument produces mapped value`` (value: FeedEntry) =
-        
+
         let result = Models.toDocument value
 
         result.uri |> should equal value.uri
