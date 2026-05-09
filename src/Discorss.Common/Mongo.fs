fee1aed24c2f26a36970ac77461cdeb749ab5f81
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-10 00:28:22 +0100

    style

--- src/Discorss.Common/Mongo.fs
+++ src/Discorss.Common/Mongo.fs
@@ -10,9 +10,20 @@ module MongoBson =
 
     let value (value: 'a) = BsonValue.Create(value)
 
-    let valueDate (value: DateTime) = 
-        let conv (x: DateTime) = new DateTime(x.Year, x.Month, x.Day, x.Hour, x.Minute, x.Second, x.Millisecond, x.Microsecond, DateTimeKind.Utc)
-        
+    let valueDate (value: DateTime) =
+        let conv (x: DateTime) =
+            new DateTime(
+                x.Year,
+                x.Month,
+                x.Day,
+                x.Hour,
+                x.Minute,
+                x.Second,
+                x.Millisecond,
+                x.Microsecond,
+                DateTimeKind.Utc
+            )
+
         BsonValue.Create(conv value)
 
     let setProperty (key: string) (value: BsonValue) (doc: BsonDocument) =
