7de50e43b64841458a55b9d501b15202d76d32d5
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-12 23:10:33 +0100

    DocStats actor message

--- src/Discorss.Ingestion/ActorMessages.fs
+++ src/Discorss.Ingestion/ActorMessages.fs
@@ -11,6 +11,8 @@ type ActorMessage =
     | FeedEntry of entry: Discorss.Feeds.FeedEntry
     | Document of docs: Discorss.Documents.Document
     | DocumentNotification of uri: string
+    | DocumentStatistics of stats: Discorss.Documents.DocumentStatistics
     | IndexDocument of doc: Discorss.Documents.Document
     | ActorStats of stats: Stats
     | PollQueue of queueName: string
+    
