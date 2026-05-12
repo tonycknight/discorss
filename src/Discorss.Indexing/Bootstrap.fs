646073aafc0acbd76404e818379640a3e984ab26
Tony Knight <tonycknight@users.noreply.github.com>
2026-05-12 23:43:21 +0100

    remove indexing projects

--- src/Discorss.Indexing/Bootstrap.fs
+++
@@ -1,8 +0,0 @@
-namespace Discorss.Indexing
-
-open Microsoft.Extensions.DependencyInjection
-
-module Bootstrap =
-    let services (services: IServiceCollection) =
-        services
-            //.AddSingleton<Discorss.Indexing.IDocumentStatsWriter, Discorss.Indexing.StubDocumentStatsWriter>()
